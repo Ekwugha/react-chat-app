55407698fc15936a38e16a598a5a8158959feebc
Ekwugha <elloekwugha@gmail.com>
2021-11-19 16:20:45 +0100

    messageform

--- src/components/TheirMessage.jsx
+++ src/components/TheirMessage.jsx
@@ -1,5 +1,5 @@
 const TheirMessage = ({ lastMessage, message }) => {
-    const isFirstMessageByUser = !lastMessage || lastMessage.sender.username !=== message.sender.username
+    const isFirstMessageByUser = !lastMessage || lastMessage.sender.username !== message.sender.username
     return (
         <div className="message-row">
             {isFirstMessageByUser && (
@@ -7,21 +7,21 @@ const TheirMessage = ({ lastMessage, message }) => {
                     className="message-avatar"
                     style={{backgroundImage: `url($[message?.sender?.avatar})`}}
                 />
-                (message?.attachments?.length > 0) 
-                   ? return(
-                        <img 
-                            src={message.attachments[0].file}
-                            alt="message-attachment"
-                            className="message-image"
-                            style={{ marginLeft: isFirstMessageByUser ? '4px' : '48px' }}
-                        />
-                    ) : (
-                        <div className="message" style={{ float: 'left', backgroundColor: '#CABCDC', marginLeft: isFirstMessageByUser ? '4px' : '48px' }} >
-                            {message.text}
-                        </div>
-                    )
-                }
             )}
+            {message?.attachments?.length > 0 
+                ? (
+                    <img 
+                        src={message.attachments[0].file}
+                        alt="message-attachment"
+                        className="message-image"
+                        style={{ marginLeft: isFirstMessageByUser ? '4px' : '48px' }}
+                    />
+                ) : (
+                    <div className="message" style={{ float: 'left', backgroundColor: '#CABCDC', marginLeft: isFirstMessageByUser ? '4px' : '48px' }} >
+                        {message.text}
+                    </div>
+                )
+            }
         </div>
     )
 }
