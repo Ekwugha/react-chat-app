e701473232d3857cde701a02648957d3fc4df98d
Ekwugha <elloekwugha@gmail.com>
2021-11-19 17:05:27 +0100

    created the login form

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import { ChatEngine } from "react-chat-engine";
+import Modal from './components/LoginForm';
 import ChatFeed from './components/ChatFeed';
 import './App.css'
 
-
+const projectID = "7950e59b-dabb-418d-8af9-b50e1293b1c7"
 
 const App = () => {
+
+  if(!localStorage.getItem('username')) return <Modal />
+
   return (
     <ChatEngine 
       height="100vh"
-      projectID="7950e59b-dabb-418d-8af9-b50e1293b1c7"
-      userName="Elo"
-      userSecret="123123"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
       renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />} 
     />
   );
